095d6389988caa6177b6948db09fb427d6e55167
Azyman <azorykavhuru@gmail.com>
2022-11-08 19:23:30 +0200

    Minor changes for taking screenshot on BaseTest.java

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -12,7 +12,6 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
 import java.io.IOException;
