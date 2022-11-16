e1bdaa907ca1fc7294839b583eeb5663653cf2aa
Azyman <115725363+Azyman@users.noreply.github.com>
2022-11-16 11:41:53 +0200

    Update Page.java

--- src/main/java/Pages/Page.java
+++ src/main/java/Pages/Page.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Page {
     public WebDriver driver;
     public WebDriverWait wait;
+    //Testing webhooks
 
     public Page(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
