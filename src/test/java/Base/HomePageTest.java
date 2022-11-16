9ed8b5c048ec3d00e2100727f3c186961defe29f
Azyman <azorykavhuru@gmail.com>
2022-11-16 17:19:56 +0200

    testing jenkis webhook

--- src/test/java/Base/HomePageTest.java
+++ src/test/java/Base/HomePageTest.java
@@ -17,6 +17,7 @@ public class HomePageTest extends BaseTest{
         Assert.assertEquals(title,"Protractor practice website - WebTables");
     }
 
+    //This is the second data provider
     @Test(priority = 2)
     public void addUser1(){
         page.getInstance(HomePage.class).addUser(By.xpath("//i[@ng-class='iconClass']"));
